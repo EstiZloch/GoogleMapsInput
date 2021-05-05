3c8e21068d002c488a47bf46459eed0cf327e432
Esther <zloch.rachel@gmail.com>
2021-05-05 20:06:36 +0300

    googleMapsInput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'new-project';
+    //Local Variable defined
+    formattedaddress=" ";
+    options={
+      componentRestrictions:{
+        country:["IL"]
+      }
+    }
+    title = 'rou';
+      public AddressChange(address: any) {
+        //setting address from API to local variable
+         this.formattedaddress=address.formatted_address;
+         this.formattedaddress=address.
+      }
 }
